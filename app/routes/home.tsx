a320f2e25d95e46700148f671e6563522c1bd0d1
arno-o <arno.oosterlynck@student.howest.be>
2025-06-06 13:57:42 +0200

    Project Setup

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,12 @@
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "Abby App" },
+    { name: "description", content: "An interactive experience for the Abby museum in Kortrijk." },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return;
 }

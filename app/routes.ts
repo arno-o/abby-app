21fed6c69c03c516b19e491e2feacc05f223cc9e
arno-o <arno.oosterlynck@student.howest.be>
2025-06-11 14:19:35 +0200

    Applied routes

--- app/routes.ts
+++ app/routes.ts
@@ -2,7 +2,13 @@ import { type RouteConfig, index, route, layout } from "@react-router/dev/routes
 
 export default [
     layout("layouts/mainLayout.tsx", [
-        index("routes/home.tsx")
+        index("routes/home.tsx"),
+        
+        route("/live", "pages/live.tsx"),
+        route("/community", "pages/community.tsx"),
+        route("/create", "pages/create.tsx"),
+        route("/schedule", "pages/schedule.tsx"),
+        route("/profile", "pages/profile.tsx")
     ]),
 
     layout("layouts/onboardLayout.tsx", [

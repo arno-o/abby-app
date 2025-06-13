ec3465c71975a35bf7231f8127f988e9224cfd18
arno-o <arno.oosterlynck@student.howest.be>
2025-06-13 15:21:29 +0200

    Refactor ScheduleCard and LocationIcon components to support dynamic styling based on type; update Schedule page to filter activities and expos.

--- app/components/ScheduleCard.tsx
+++ app/components/ScheduleCard.tsx
@@ -2,18 +2,18 @@ import LocationIcon from "./icons/Location";
 import BookmarkIcon from "./icons/BookmarkIcon";
 import AbbyArrowTilted from "./icons/AbbyArrowTilted";
 
-const ScheduleCard = (props: { timespan: string, title: string, location: string, image_url: string; bookmarked?: boolean }) => {
+const ScheduleCard = (props: { timespan: string, title: string, location: string, image_url: string; bookmarked?: boolean; type: string }) => {
     return (
-        <div className="flex flex-col bg-abby-green my-4 p-4 gap-4">
+        <div className={`flex flex-col my-4 p-4 gap-4 ${props.type == 'activity' ? "bg-abby-green" : "bg-abby-purple"}`}>
             <img src={`app/assets/img/schedule/schedule_item_${props.image_url}.png`} />
             <div className="flex justify-between text-xl">
                 <div className="flex flex-col gap-2">
-                    <div className="text-[#B8E6D5]">
+                    <div className={`${props.type == "activity" ? "text-[#B8E6D5]": "text-[#783690]"}`}>
                         <span className="font-bold">{props.timespan}</span>
                     </div>
                     <p className="text-2xl">{props.title}</p>
-                    <div className="flex items-center gap-2 text-[#B8E6D5]">
-                        <LocationIcon />
+                    <div className={`flex items-center gap-2 ${props.type == "activity" ? "text-[#B8E6D5]": "text-[#783690]"}`}>
+                        <LocationIcon color={props.type == "activity" ? "#B8E6D5": "#783690"}/>
                         {props.location}
                     </div>
                 </div>
--- app/pages/schedule.tsx
+++ app/pages/schedule.tsx
@@ -8,17 +8,16 @@ import { scheduleActivityItems } from "~/data/data";
 
 const Schedule = () => {
     const [dateNavFirst, setDateNavFirst] = useState(true);
+    const [activityNavFirst, setActivityNavFirst] = useState(true);
 
-    const handleDateNav = () => {
-        if (dateNavFirst) { setDateNavFirst(false) } else if (!dateNavFirst) { setDateNavFirst(true) }
-    }
+    const activeActivity = `bg-abby-blue text-white`;
 
     return (
         <>
             <div className="p-5 bg-abby-yellow flex flex-col gap-4">
                 <div className="flex">
-                    <button onClick={handleDateNav} className={`${dateNavFirst ? "border-b-4" : "border-b-2"} w-full transition-all`}><div className="text-center p-5 text-xl">By Date</div></button>
-                    <button onClick={handleDateNav} className={`${!dateNavFirst ? "border-b-4" : "border-b-2"} w-full transition-all`}><div className=" text-center p-5 text-xl">Show All</div></button>
+                    <button onClick={() => setDateNavFirst(true)} className={`${dateNavFirst ? "border-b-4" : "border-b-2"} w-full transition-all`}><div className="text-center p-5 text-xl">By Date</div></button>
+                    <button onClick={() => setDateNavFirst(false)} className={`${!dateNavFirst ? "border-b-4" : "border-b-2"} w-full transition-all`}><div className=" text-center p-5 text-xl">Show All</div></button>
                 </div>
 
                 <div className="flex items-center justify-between">
@@ -30,8 +29,8 @@ const Schedule = () => {
                     </div>
 
                     <div className="flex gap-3">
-                        <AbbyArrowLeft size={30} disabled/>
-                        <AbbyArrowRight size={30}/>
+                        <AbbyArrowLeft size={30} disabled />
+                        <AbbyArrowRight size={30} />
                     </div>
                 </div>
 
@@ -64,9 +63,18 @@ const Schedule = () => {
                 </div>
             </div>
 
-            {scheduleActivityItems.items.map((item) => (
-                <ScheduleCard key={item.image_url} timespan={item.timespan} title={item.title} location={item.location} image_url={item.image_url}/>
-            ))}
+            <div className="flex justify-between bg-stone-200 m-4">
+                <button onClick={() => setActivityNavFirst(true)} className={`transition-all m-3 text-center w-full p-4 text-abby-blue text-xl ${activityNavFirst ? activeActivity : ""}`}>Activities</button>
+                <button onClick={() => setActivityNavFirst(false)} className={`transition-all m-3 text-center w-full p-4 text-abby-blue text-xl ${!activityNavFirst ? activeActivity : ""}`}>Expo's</button>
+            </div>
+
+            <div className="px-4 pb-4">
+                {scheduleActivityItems.items
+                    .filter(item => activityNavFirst ? item.type === "activity" : item.type === "expo")
+                    .map((item) => (
+                        <ScheduleCard key={item.image_url} timespan={item.timespan} title={item.title} location={item.location} image_url={item.image_url} type={item.type} />
+                    ))}
+            </div>
 
             <NavBar />
         </>

183685672b53173fe115f8834b3c165e34a221a8
lunacoppin <luna.coppin@student.howest.be>
2025-06-15 16:40:49 +0200

    create page

---
+++ app/components/icons/AbbyArrowSelect.tsx
@@ -0,0 +1,9 @@
+const AbbyArrowSelect = () => {
+    return (
+        <svg width="16" height="17" viewBox="0 0 16 17" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M5.33333 0H0L5.33333 17H10.9474L16 0H10.6667L8.14035 14.6949L5.33333 0Z" fill="#BB7CD2" />
+        </svg>
+    );
+}
+
+export default AbbyArrowSelect;

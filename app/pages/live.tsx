b3e57305d45ac60061f43cd6a215014a9d297825
arno-o <arno.oosterlynck@student.howest.be>
2025-06-15 18:02:00 +0200

    Random blobs on page

--- app/pages/live.tsx
+++ app/pages/live.tsx
@@ -1,8 +1,13 @@
 import NavBar from "~/components/NavBar";
+import BlobsContainer from "~/components/BlobsContainer";
 
 const Live = () => {
     return (
         <>
+            <div className="">
+                <BlobsContainer count={8} />
+            </div>
+
             <NavBar />
         </>
     );
